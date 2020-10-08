0ce91328021241aafcd6c0a5e75087afb0da8012
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-08 15:31:45 +0530

    [Jitendra] Added: Max Float using Generics with Junit testing

--- src/main/java/com/bridgelabz/genericstest/TestMaximum.java
+++ src/main/java/com/bridgelabz/genericstest/TestMaximum.java
@@ -10,10 +10,19 @@ public class TestMaximum {
             max = num3;
         return max;
     }
-
-    public static void main(String[] args) {
+	//function for finding max float number among three numbers using generics
+	public static Float findMax(Float num1, Float num2, Float num3) {
+        Float max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+        if (num3.compareTo(max) > 0)
+            max = num3;
+        return max;
+    }
+	public static void main(String[] args) {
         System.out.println("Welcome to Test Maximum");
-        System.out.println("Max among these value is: "+findMax(1,2,3));
+        System.out.println("Max Integer among these value is: "+findMax(1,2,3));
+        System.out.println("Max float among these value is: "+findMax(1.0f,2.0f,3.0f));
     }
 
 }
--- src/test/java/com/bridgelabz/genericstest/TestMaximumTest.java
+++ src/test/java/com/bridgelabz/genericstest/TestMaximumTest.java
@@ -1,32 +1,42 @@
 package com.bridgelabz.genericstest;
 import java.util.*;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 public class TestMaximumTest {
 	@Test
     public void first_num_max_return_first() {
-        TestMaximum t = new TestMaximum();
-        Integer max = t.findMax(7, 6, 5);
+        Integer max = TestMaximum.findMax(7, 6, 5);
         Assert.assertSame(7, max);
     }
-
-    @Test
+	@Test
     public void second_num_max_return_second() {
-        TestMaximum t = new TestMaximum();
-        Integer max = t.findMax(6, 7, 5);
+        Integer max = TestMaximum.findMax(6, 7, 5);
         Assert.assertSame(7, max);
     }
-
-    @Test
+	@Test
     public void third_num_max_return_third() {
-        TestMaximum t = new TestMaximum();
-        Integer max = t.findMax(5, 6, 7);
+        Integer max = TestMaximum.findMax(5, 6, 7);
         Assert.assertSame(7, max);
     }
     @Test
     public void check_max_number_is_true_when_numbers_are_same() {
-        TestMaximum t = new TestMaximum();
-        Integer max = t.findMax(1,1,1);
+        Integer max = TestMaximum.findMax(1,1,1);
         Assert.assertSame(1, max);
+    }
+    @Test
+    public void first_float_max_return_first() {
+        Float max = TestMaximum.findMax(7.01f, 6.01f, 5.01f);
+        Assert.assertThat(max,CoreMatchers.equalTo(7.01f));
+    }
+    @Test
+    public void second_float_max_return_second() {
+        Float max = TestMaximum.findMax(5.01f, 7.01f, 6.01f);
+        Assert.assertThat(max,CoreMatchers.equalTo(7.01f));
+    }
+    @Test
+    public void third_float_max_return_third() {
+        Float max = TestMaximum.findMax(5.01f, 6.01f, 7.01f);
+        Assert.assertThat(max,CoreMatchers.equalTo(7.01f));
     }
    }
