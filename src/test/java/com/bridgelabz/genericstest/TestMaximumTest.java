7ba8abfd22a0763e33911e8f0069f40f6780916b
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-08 15:05:44 +0530

    [Jitendra]Refactored: Added test case for max number testing

--- src/test/java/com/bridgelabz/genericstest/TestMaximumTest.java
+++ src/test/java/com/bridgelabz/genericstest/TestMaximumTest.java
@@ -4,10 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 public class TestMaximumTest {
 	@Test
-    public void check_max_number_is_true() {
+    public void first_num_max_return_first() {
         TestMaximum t = new TestMaximum();
-        Integer max = t.findMax(1,2,3);
-        Assert.assertSame(3, max);
+        Integer max = t.findMax(7, 6, 5);
+        Assert.assertSame(7, max);
+    }
+
+    @Test
+    public void second_num_max_return_second() {
+        TestMaximum t = new TestMaximum();
+        Integer max = t.findMax(6, 7, 5);
+        Assert.assertSame(7, max);
+    }
+
+    @Test
+    public void third_num_max_return_third() {
+        TestMaximum t = new TestMaximum();
+        Integer max = t.findMax(5, 6, 7);
+        Assert.assertSame(7, max);
     }
     @Test
     public void check_max_number_is_true_when_numbers_are_same() {
